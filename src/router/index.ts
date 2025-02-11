81c86625822cc364ea6f1733673992f33bd42116
wazest <stefan.waldner@spoud.io>
2025-02-11 19:48:26 +0100

    add routing and finance page2

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Dashboard from '../components/Dashboard.vue'
+import Finance from '../components/Finance.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'Dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/finance',
+      name: 'Finance',
+      component: Finance
+    }
+  ]
+})
+
+export default router
